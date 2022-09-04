8ecd98a5a1a76572ba7f0821d6c4575f18f309c2
kdodson2024 <kdodson2024@chadwickschool.org>
2022-09-04 15:13:33 -0700

    Initial commit

---
+++ Assets/CamControl.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamControl : MonoBehaviour
+{
+    public float rot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, Vector3.forward * 5.2f);
+        rot += -4 * Input.GetAxis("Mouse Y");
+        rot = Mathf.Clamp(rot, -89.5f, 89.5f);
+        transform.eulerAngles = new Vector3(rot, transform.eulerAngles.y, transform.eulerAngles.z);
+    }
+    public GameObject GrabObjCheck(Vector3 vectorToTarget){
+        RaycastHit hit;
+        Ray checkRay = new Ray (transform.position, vectorToTarget * 1.5f);
+        if(Physics.Raycast(checkRay, out hit, 10.4f)){
+            Debug.Log(hit.collider.gameObject);
+            if(hit.collider.tag == "grab"){
+                return hit.collider.gameObject;
+            }
+            else{
+                return null;
+            }
+        }
+        Debug.Log("hit nothing");
+        return null;
+    }
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Rigidbody rigid;
+    public bool onGround;
+    RaycastHit ground;
+    public CapsuleCollider col;
+    public GameObject grabPoint;
+    public GameObject mainCamera;
+    public GameObject grabbedObj;
+    public Vector2 speedBoost;
+    public Vector3 aBitAbove;
+    public GameObject forwardTarget;
+    public GameObject rightTarget;
+    public Vector3 toForward;
+    public Vector3 toRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // rigid.velocity = new Vector3(rigid.velocity.x, rigid.velocity.y, Input.GetAxis("Vertical") * 5);
+        rigid.velocity = new Vector3(0, rigid.velocity.y, 0);
+        toForward = forwardTarget.transform.position - transform.position;
+        rigid.velocity += (toForward * 10 * Input.GetAxis("Vertical"));
+        toRight = rightTarget.transform.position - transform.position;
+        rigid.velocity += (toRight * 10 * Input.GetAxis("Horizontal"));
+        // transform.Translate(Input.GetAxis("Horizontal") * 5 * Time.deltaTime, 0, Input.GetAxis("Vertical") * 5 * Time.deltaTime);
+        // transform.Translate(speedBoost.x * Time.deltaTime, 0, speedBoost.y * Time.deltaTime);
+        rigid.velocity += (toForward * speedBoost.y);
+        rigid.velocity += (toRight * speedBoost.x);
+        aBitAbove = transform.position + Vector3.up;
+        Ray landingRay = new Ray (transform.position, Vector2.down);
+        if(Input.GetKey(KeyCode.LeftControl))
+            onGround = Physics.Raycast(landingRay, out ground, 0.4f);
+        else
+            onGround = Physics.SphereCast(transform.position, 0.8f, Vector3.down, out ground, 1.5f);
+        Debug.Log(ground);
+        if(onGround && Input.GetKeyDown(KeyCode.Space))
+            rigid.AddRelativeForce(0, 6000, 0);
+        if(Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0){
+            if(speedBoost != Vector2.zero){
+                if(speedBoost.x >= 0)
+                    speedBoost = new Vector2(speedBoost.x - Mathf.Min(speedBoost.x, 10 * Time.deltaTime), speedBoost.y);
+                else
+                    speedBoost = new Vector2(speedBoost.x - Mathf.Max(speedBoost.x, -10 * Time.deltaTime), speedBoost.y);
+                if(speedBoost.y >= 0)
+                    speedBoost = new Vector2(speedBoost.x, speedBoost.y - Mathf.Min(speedBoost.y, 10 * Time.deltaTime));
+                else
+                    speedBoost = new Vector2(speedBoost.x, speedBoost.y - Mathf.Max(speedBoost.y, -10 * Time.deltaTime));
+            }
+        }
+        if(onGround){
+            if(speedBoost != Vector2.zero){
+                if(speedBoost.x >= 0)
+                    speedBoost = new Vector2(speedBoost.x - Mathf.Min(speedBoost.x, 6 * Time.deltaTime), speedBoost.y);
+                else
+                    speedBoost = new Vector2(speedBoost.x - Mathf.Max(speedBoost.x, -6 * Time.deltaTime), speedBoost.y);
+                if(speedBoost.y >= 0)
+                    speedBoost = new Vector2(speedBoost.x, speedBoost.y - Mathf.Min(speedBoost.y, 6 * Time.deltaTime));
+                else
+                    speedBoost = new Vector2(speedBoost.x, speedBoost.y - Mathf.Max(speedBoost.y, -6 * Time.deltaTime));
+            }
+        }
+        if(Input.GetKey(KeyCode.LeftControl)){
+            col.height = 1.25f;
+            col.center = new Vector3(0, 0.375f, 0);
+            if(!onGround){
+                speedBoost = new Vector2(speedBoost.x + Input.GetAxis("Horizontal") * 5 * Time.deltaTime, speedBoost.y + Input.GetAxis("Vertical") * 5 * Time.deltaTime);
+            }
+        }
+        else{
+            col.height = 2.0f;
+            col.center = new Vector3(0, 0, 0);
+        }
+        if(Input.GetKeyDown(KeyCode.E)){
+            if(grabbedObj == null){
+                Vector3 vectorToTarget = grabPoint.transform.position - mainCamera.transform.position;
+                grabbedObj = mainCamera.GetComponent<CamControl>().GrabObjCheck(vectorToTarget * 1.1f);
+            }
+            else{
+                grabbedObj = null;
+            }
+        }
+        if(grabbedObj != null){
+            // grabbedObj.transform.parent = grabPoint.transform;
+            // grabbedObj.transform.position = grabPoint.transform.position;
+            grabbedObj.GetComponent<Rigidbody>().velocity = (grabPoint.transform.position - grabbedObj.transform.position) * 10;
+        }
+    }
+}
